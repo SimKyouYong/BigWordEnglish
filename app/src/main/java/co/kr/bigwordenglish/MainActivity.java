e17ab1e672827cef18c9fa2e83461b3423b76fb0
심규용 <SKY@simgyuyongui-MacBook-Pro.local>
2017-04-18 14:01:59 +0900

    sky 테스트 깃

--- app/src/main/java/co/kr/bigwordenglish/MainActivity.java
+++ app/src/main/java/co/kr/bigwordenglish/MainActivity.java
@@ -2,7 +2,7 @@ package co.kr.bigwordenglish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//SKY 테스트 git
 public class MainActivity extends AppCompatActivity {
 
     @Override
