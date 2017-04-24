6223f284d39ce4e418b4779dd6b7d9ae9cf5e8ad
SKY <snap0425>
2017-04-24 11:10:34 +0900

    1.디비다운로드 기능

--- app/src/main/java/co/kr/bigwordenglish/IntroActivity.java
+++ app/src/main/java/co/kr/bigwordenglish/IntroActivity.java
@@ -1,12 +1,33 @@
 package co.kr.bigwordenglish;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+import co.kr.bigwordenglish.common.Check_Preferences;
+import co.kr.bigwordenglish.common.CommonUtil;
+import co.kr.bigwordenglish.common.MySQLiteOpenHelper;
 import co.kr.bigwordenglish.util.Log;
+import co.kr.sky.AccumThread;
 
 
 /**
@@ -16,8 +37,13 @@ import co.kr.bigwordenglish.util.Log;
  */
 
 public class IntroActivity extends AppCompatActivity {
+    CommonUtil dataSet = CommonUtil.getInstance();
 
 	ImageView introimg;
+    private AccumThread mThread;
+    private  MySQLiteOpenHelper vc;
+    private Map<String, String> map = new HashMap<String, String>();
+    private float local_Ver, Server_Ver;
 
 	private Handler mIntroHandler = new Handler();
 
@@ -29,44 +55,20 @@ public class IntroActivity extends AppCompatActivity {
 
 		init();
 
-
-		introimg.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-//				if (!PermissionUtils.canAccessPhone(IntroActivity.this) ) {  // 폰 정보 권한이 없다면
-//					ActivityCompat.requestPermissions(IntroActivity.this, PermissionUtils.PHONE_PERMS, CommonData.PERMISSION_REQUEST_PHONE_STATE);
-//				} else {
-					mIntroHandler.postDelayed(mIntroRunnable, 1000);
-//					requestAppInfo();
-//				}
-			}
-		}, 1000);
+//
+//		introimg.postDelayed(new Runnable() {
+//			@Override
+//			public void run() {
+////				if (!PermissionUtils.canAccessPhone(IntroActivity.this) ) {  // 폰 정보 권한이 없다면
+////					ActivityCompat.requestPermissions(IntroActivity.this, PermissionUtils.PHONE_PERMS, CommonData.PERMISSION_REQUEST_PHONE_STATE);
+////				} else {
+//					mIntroHandler.postDelayed(mIntroRunnable, 1000);
+////					requestAppInfo();
+////				}
+//			}
+//		}, 1000);
 
 	}
-
-//	/**
-//	 * 앱 최초 실행시 바로가기 아이콘 추가
-//	 */
-//	private void addShortcut() {
-//		Log.i("addShortcut()");
-//		//Adding shortcut for MainActivity
-//		//on Home screen
-//		Intent shortcutIntent = new Intent(getApplicationContext(), IntroActivity.class);
-//		shortcutIntent.setAction(Intent.ACTION_MAIN);
-//
-//		Intent addIntent = new Intent();
-//		addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-//		addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
-//		addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
-//				Intent.ShortcutIconResource.fromContext(getApplicationContext(),
-//						R.mipmap.launcher));
-//		addIntent.putExtra("duplicate", false);
-//		addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-//		getApplicationContext().sendBroadcast(addIntent);
-//
-//		CommonData.getInstance().setShortCut(true);
-//	}
-
 //	/**
 //	 * 초기화
 //	 */
@@ -75,55 +77,135 @@ public class IntroActivity extends AppCompatActivity {
 		introimg	=	(ImageView)	findViewById(R.id.intro_image);
 
 
+        vc = new MySQLiteOpenHelper(this);
+
+        map.put("url", dataSet.SERVER + "Version.php");
+        // 스레드 생성
+        mThread = new AccumThread(this, mAfterAccum, map, 0, 0, null);
+        mThread.start(); // 스레드 시작!!
 	}
+    Handler mAfterAccum = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.arg1 == 0) {
+                String res = (String) msg.obj;
+                //Log.e("CHECK", "RESULT  -> " + res);
+                String ver = Check_Preferences.getAppPreferences(getApplicationContext(), "version");
+                //Log.e("SKY", "ver  -> " + ver);
+                if (!ver.equals("")) {
+                    local_Ver = Float.parseFloat(ver);
+                    Server_Ver = Float.parseFloat(res);
+                    //Log.e("SKY", "local_Ver :: " + local_Ver);
+                    //Log.e("SKY", "Server_Ver :: " + Server_Ver);
+                    if (local_Ver < Server_Ver) {
+                        // 다운로드
+                        new DownloadFileFullAsync(IntroActivity.this).execute(dataSet.SERVER + "police_db.db");
+                    } else {
+                        MainMove();
+                    }
+                } else {
+                    // 최초버전.. 무조건 다운로드
+                    new DownloadFileFullAsync(IntroActivity.this).execute(dataSet.SERVER + "police_db.db");
+                }
+            }
+        }
+    };
+    private void MainMove(){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+    public class DownloadFileFullAsync extends
+            AsyncTask<String, String, String> {
 
-	/**
-	 * 프롤로그 화면이동 런어블
-	 */
-	private Runnable mIntroRunnable = new Runnable() {
+        private ProgressDialog mDlg;
+        private Context mContext;
 
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			Log.i("run");
-			Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-			startActivity(intent);
-			IntroActivity.this.finish();
-		}
-	};
+        public DownloadFileFullAsync(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDlg = new ProgressDialog(mContext, AlertDialog.THEME_HOLO_LIGHT);
+            mDlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mDlg.setMessage("로딩중");
+            mDlg.show();
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            int count = 0;
+            try {
+                String str = dataSet.SERVER + "police_db.db";
+                String DEFAULT_FILE_PATH = dataSet.Local_Path + "/police_db.db";
+                Log.e("SKY", "STR :: " + str);
+
+                URL url = new URL(str);
+                URLConnection conexion = url.openConnection();
+                conexion.setRequestProperty("Accept-Encoding", "identity"); // <--- Add this line
+
+                conexion.connect();
+
+                int lenghtOfFile = conexion.getContentLength();
+                Log.e("SKY", "Lenght of file: " + lenghtOfFile);
+
+                File file = new File(dataSet.Local_Path);
+                if (!file.exists()) { // 원하는 경로에 폴더가 있는지 확인
+                    Log.e("SKY", "폴더 생성");
+                    file.mkdirs();
+                }
 
 
+                InputStream input = new BufferedInputStream(url.openStream());
+                OutputStream output = new FileOutputStream(DEFAULT_FILE_PATH);
 
-	// 퍼미션 권한 획득 리퀘스트
-//	@Override
-//	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//
-//		switch(requestCode) {
-//			case CommonData.PERMISSION_REQUEST_PHONE_STATE:
-//				if (PermissionUtils.canAccessPhone(this)) {	// 폰 권한을 획득했다면
-//					mIntroHandler.postDelayed(mIntroRunnable, CommonData.INTRO_POST_DELAYED);
-//				}
-//				else {
-//					Log.i("권한 획득 거부 or 취소");
-//					mDialog = new CustomAlertDialog(this, CustomAlertDialog.TYPE_A);
-//					mDialog.setTitle(getString(R.string.popup_dialog_a_type_title));
-//					mDialog.setContent(getString(R.string.popup_dialog_permission_content));
-//					mDialog.setPositiveButton(getString(R.string.popup_dialog_button_confirm), new CustomAlertDialogInterface.OnClickListener() {
-//
-//						@Override
-//						public void onClick(CustomAlertDialog dialog, Button button) {
-//							mDialog.dismiss();
-//							finish();
-//						}
-//					});
-//					mDialog.show();
-//				}
-//				break;
-//			default:
-//				Log.i("onRequestPermissionsResult default");
-//				super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//				break;
-//		}
-//	}
+                byte data[] = new byte[1024];
+                long total = 0;
+                while ((count = input.read(data)) != -1) {
+                    Log.e("SKY", "total :: " + total);
+                    total += count;
+                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... progress) {
+            // Log.e("SKY" , "progress size :: " + progress.length);
+            // Log.e("SKY" , "progress 0 :: " + progress[0]);
+            mDlg.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        protected void onPostExecute(String unused) {
+            mDlg.dismiss();
+            Check_Preferences.setAppPreferences(IntroActivity.this, "version","" + Server_Ver);
+            MainMove();
+        }
+    }
+
 
 }
--- app/src/main/java/co/kr/bigwordenglish/common/CommonUtil.java
+++ app/src/main/java/co/kr/bigwordenglish/common/CommonUtil.java
@@ -1,93 +1,20 @@
 package co.kr.bigwordenglish.common;
 
-import android.app.Activity;
-
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-
-
-/*
- * 怨듯넻�븿�닔 諛� �쟾泥댄겢�옒�뒪�뿉�꽌 蹂��닔 �젒洹쇳븯湲� �쐞�븳 怨듯넻 蹂��닔�뱾 �뀑�똿 �겢�옒�뒪
- * */
 public class CommonUtil {
 	private static CommonUtil _instance;
-	public int EA;
-	public int HomeBtn;
-	
-	public ArrayList<Activity> av = new ArrayList<Activity>();					//Activity 를 담는다.
+
 
 	public String Local_Path;
-	public String PHONE;
 	public String SERVER;
-	public String SERVER_IMG;
-	public String SERVER_ADIMG;
-	public String SERVER_E_IMG;
-	public String SERVER_F_IMG;
-	public String SERVER_FITIMG;
-	public String SERVER_MP3;
-	public String SERVER_DB;
-	public String SERVER_MY_IMG;
-	public String COSS_FOOD;
-	
-	
-	public String LOGIN_MEMBER_GEUST;
-	
-	public ArrayList<Boolean> mp3_flag;
-	public ArrayList<Boolean> mp3_detail_flag;
-	
-	public boolean Activity_flag = false;
-	public boolean Agree_Check;
-	public boolean EXIT;
-	public String REG_ID;
-	public String PROJECT_ID;
-	public String CHOO;
-	public String MY_EA;
-	public String POINT;
-	public String MY_IMG;
 
-	
-	public String KEY_INDEX;
-	public String NAME;
-	public String USER_ID;
-	public String USER_PW;
-	public String USER_EMAIL;
-	public String USER_PHONE;
-	public String USER_BIRTH;
 
 	static {
 		_instance = new CommonUtil();
 		try {								 
-			_instance.PROJECT_ID = 	   		"1072720857084";
-			_instance.REG_ID = 	   		"";
-			_instance.LOGIN_MEMBER_GEUST = 	   		"";
-			_instance.MY_IMG = 	   		"";
-			_instance.Local_Path = 	   		"/data/data/co.kr.app.helloweurope/databases"+ "/Hellow/";
-			//_instance.Local_Path = 	   		Environment.getExternalStorageDirectory().getAbsolutePath()+ "/Hellow/";
-			
-			_instance.mp3_flag = 	   		new ArrayList<Boolean>();
-			_instance.mp3_detail_flag = 	   		new ArrayList<Boolean>();
-			_instance.KEY_INDEX = 	   		"";
-			_instance.NAME = 	   			"";
-			_instance.USER_ID = 	   		"";
-			_instance.USER_PW = 	   		"";
-			_instance.USER_EMAIL = 	   		"";
-			_instance.USER_PHONE = 	   		"";
-			_instance.USER_BIRTH = 	   		"";
-			_instance.POINT = 	   		"";
-
-			_instance.EXIT = 	   		false;
-			_instance.PHONE = 	   		"";
-			_instance.SERVER = 	   		"http://coaineu.cafe24.com/Hellow/php/";
-			_instance.SERVER_IMG = 	   		"http://coaineu.cafe24.com/Hellow/hellowtalk_img/";
-			_instance.SERVER_E_IMG = 	   		"http://coaineu.cafe24.com/Hellow/EuropePath_IMG/";
-			_instance.SERVER_F_IMG = 	   		"http://coaineu.cafe24.com/Hellow/Food_IMG/";
-			_instance.SERVER_FITIMG = 	   		"http://coaineu.cafe24.com/Hellow/fitimg/";
-			_instance.SERVER_MP3 = 	   		"http://coaineu.cafe24.com/Hellow/mp3/";
-			_instance.SERVER_DB = 	   		"http://coaineu.cafe24.com/Hellow/DB/hellow_db.db";
-			_instance.SERVER_MY_IMG = 	   		"http://coaineu.cafe24.com/Hellow/MY_IMG/";
-			_instance.COSS_FOOD = 	   		"http://coaineu.cafe24.com/Hellow/Coss_Food/";
-			_instance.SERVER_ADIMG = 	   		"http://coaineu.cafe24.com/Hellow/AD_IMG/";
+			_instance.SERVER = 	   		"http://snap40.cafe24.com/BigWordEgs/";
 
 			
 		} catch (Exception e) {
